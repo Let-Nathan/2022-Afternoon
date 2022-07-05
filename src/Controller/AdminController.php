d4ca3b56ddbbf74b3c434c1901ece772eae8a753
LEA-RASOLOARISON <lea.rasoloarison@gmail.com>
2022-07-05 11:09:21 +0200

    wip

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin;
+use App\Form\AdminType;
 use App\Repository\AdminRepository;
-use Composer\DependencyResolver\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -17,4 +19,50 @@ class AdminController extends AbstractController
             'admin' => $admin->findBy(['id' => 'ASC'])
         ]);
     }
+
+    #[Route('/admin/new', name: 'admin_new')]
+    public function new(Request $request, AdminRepository $adminRepository): Response
+    {
+        $admin = new Admin();
+        $form = $this->createForm(AdminType::class, $admin);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $adminRepository->add($admin, true);
+
+            return $this->redirectToRoute('Admin/accountAdmin.html.twig');
+        }
+
+        return $this->renderForm('admin/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    /*
+
+    #[Route('/programs/{slug}/edit', name: 'edit_programs', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Program $program, EntityManagerInterface $entityManager): Response
+    {
+
+        if (!($this->getUser() == $program->getOwner())) {
+            // If not the owner, throws a 403 Access Denied exception
+            throw new AccessDeniedException('Only the owner can edit the program!');
+        }
+
+        $form = $this->createForm(ProgramType::class, $program);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('program_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('program/edit.html.twig', [
+            'program' => $program,
+            'form' => $form,
+        ]);
+    }
+
+    */
 }
---
+++ src/Form/AdminType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Admin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName')
+            ->add('firstName')
+            ->add('email')
+            ->add('phone')
+            ->add('role')
+            ->add('candidates')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Admin::class,
+        ]);
+    }
+}
