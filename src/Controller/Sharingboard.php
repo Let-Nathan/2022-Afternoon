20176b61674f45be16f05bb42b5067fb10dab864
Let-Nathan <letournel.nathan@gmail.com>
2022-07-07 02:46:23 +0200

    tt

--- src/Controller/Sharingboard.php
+++ src/Controller/Sharingboard.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/sharingboard', name: 'app_sharingboard')]
+#[Route('/sharingboard'git br, name: 'app_sharingboard')]
 class Sharingboard extends AbstractController
 {
     /**
