5f4af0ba48d83ceb53cfef9f72f96b39ba3320d5
Let-Nathan <letournel.nathan@gmail.com>
2022-07-07 02:43:25 +0200

    wip

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,25 +2,21 @@
 
 namespace App\Controller;
 
-use App\Entity\Consultation;
-use App\Entity\User;
 use App\Repository\CandidateRepository;
 use App\Repository\ConsultationRepository;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/dashboard')]
 class DashboardController extends AbstractController
 {
     /**
      * @TODO Sommes balance & dues
      * @TODO Montant des fiches
      */
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/', name: 'app_dashboard_index')]
     public function dashboard(
         CandidateRepository $candidateRepository,
         ConsultationRepository $cRepo,
--- src/Controller/Sharingboard.php
+++ src/Controller/Sharingboard.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/sharingboard', name: 'app_sharingboard')]
+#[Route('/sharingboard')]
 class Sharingboard extends AbstractController
 {
     /**
